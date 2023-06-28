85d6d7c6be82afa39a2aa05654cb82ee483df02e
kunaldhongade <kunaldhongade1@gmail.com>
2023-06-28 21:53:03 +0530

    02_var

--- 02_variables.js
+++ 02_variables.js
@@ -1,7 +1,15 @@
 "use strict"; // it is used to prevent error and works strictly
 
+// variables can store some info
+// we can use that info later
+// we can change that info later
+
 var firstName = "Dhongade"
 console.log(firstName)
 
 firstName = "Dhongade Kunal Santosh"
 console.log(firstName)
+
+var a02 = 34
+var _a = 3
+console.log(a02, 10, _a)
