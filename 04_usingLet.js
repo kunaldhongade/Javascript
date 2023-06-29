8e129d8d659eb34a07a3caa5f115cb8f09fe3a06
kunaldhongade <kunaldhongade1@gmail.com>
2023-06-29 17:59:47 +0530

    04_let

--- 04_usingLet.js
+++ 04_usingLet.js
@@ -9,5 +9,26 @@ let variable = "JavaScript";
 variable = "Typescript"
 console.log(variable);
 
+function varExample() {
+    var x = 1;
 
+    if (1) {
+        var x = 101
+        console.log(x) // 101
+    }
+    console.log(x) // 101
+}
+
+function letExample() {
+    let x = 1;
+
+    if (1) {
+        let x = 111
+        console.log(x) // 111
+    }
+
+    console.log(x) // 1
+}
+varExample()
+letExample()
 // block scope vs function scope
