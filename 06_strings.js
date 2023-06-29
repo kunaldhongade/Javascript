2c799e92096a1f3ac670affa11589a16ef2868e0
kunaldhongade <kunaldhongade1@gmail.com>
2023-06-29 19:22:24 +0530

    string

--- 06_strings.js
+++ 06_strings.js
@@ -1,6 +1,10 @@
-let variable = "JavaScript is JS"
+let str = "Kunal"
 
-console.log(variable)
-console.log(variable[2])
-console.log(variable.length);
-console.log(variable[variable.length - 4])
+// K u n a l  -> value 
+// 0 1 2 3 4  -> index
+
+console.log("value of str " + str)
+console.log("3rd indexed value of str " + str[2])
+console.log("length of str " + str.length);
+console.log("last 4th value of str " + str[str.length - 4])
+console.log("last value of str " + str[str.length - 1])
