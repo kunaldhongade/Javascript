b8b336c3cad4b3b6f44c3a7234331e749190a2d3
kunaldhongade <kunaldhongade1@gmail.com>
2023-07-01 20:10:42 +0530

    templet Str

---
+++ 11_tempStr.js
@@ -0,0 +1,7 @@
+let name = "Kunal"
+let age = 21
+let occupation = "Blockchain Developer"
+
+let aboutMe = `Hello there, I am ${name} a passionate ${occupation} and i'm ${age} year's old.`
+
+console.log(aboutMe)
