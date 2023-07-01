5b2d5c016131cb6d9d3c06c674fdf3ed818661e7
kunaldhongade <kunaldhongade1@gmail.com>
2023-07-01 19:55:46 +0530

    typeChange

---
+++ 09_TypeChange.js
@@ -0,0 +1,22 @@
+let a = 23
+let n = "kunal"
+let nnx = "234"
+
+console.log(typeof a, a, typeof n, n)
+
+// number to String
+
+let x = a + n
+console.log(typeof x, x)
+
+let xx = String(a)
+console.log(typeof xx, xx)
+
+
+// string to number
+
+let nx = +"222" // to change string to number add + before str
+console.log(typeof nx, nx);
+
+let nxx = Number(nnx)
+console.log(typeof nxx, nxx)
