ea5527bc1a492bb2045fadd319119eaf10cb2ebf
kunaldhongade <kunaldhongade1@gmail.com>
2023-07-02 16:14:50 +0530

    Loops

---
+++ 20_forLoop.js
@@ -0,0 +1,22 @@
+for (let i = 0; i <= 5; i++) {
+    console.log(i)
+}
+console.log("------------------------------------")
+
+
+for (let i = 0; i <= 5; i++) {
+    if (i == 4) {
+        break;
+
+    }
+    console.log(i)
+}
+console.log("------------------------------------")
+
+for (let i = 0; i <= 5; i++) {
+    if (i == 4) {
+        continue;
+
+    }
+    console.log(i)
+}
