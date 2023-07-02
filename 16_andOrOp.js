6717f5c8adcf1cd67fd149996da722eb37694d06
kunaldhongade <kunaldhongade1@gmail.com>
2023-07-02 14:29:13 +0530

    and Or

---
+++ 16_andOrOp.js
@@ -0,0 +1,10 @@
+let isSharingan = true
+let isUchhiha = false
+let isOrphan = true
+let isAnbu = !true
+
+if (isSharingan && !isUchhiha && isOrphan || isAnbu) {
+    console.log("You are kakashi Hatake of sharingan");
+} else {
+    console.log("You are not copy ninja!")
+}
