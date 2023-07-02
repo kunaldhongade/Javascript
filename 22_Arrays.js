cea31ed73f61ebaceefb6fdcaa9c80e17b176d2b
kunaldhongade <kunaldhongade1@gmail.com>
2023-07-02 16:25:17 +0530

    Arrays

---
+++ 22_Arrays.js
@@ -0,0 +1,9 @@
+// arrays are ordered collection of items
+// it is reference type
+// arrays works on indexing start with 0
+
+let team7 = ["Naruto", "Sasuke", "Sakura", "Sai "]
+console.log(team7[0])
+
+let bestTeam = [7, 9, 10]
+console.log(bestTeam[7])
