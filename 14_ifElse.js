1b1eb8605aee40f44a030affabe63d3396ba705f
kunaldhongade <kunaldhongade1@gmail.com>
2023-07-02 11:26:36 +0530

    if else

---
+++ 14_ifElse.js
@@ -0,0 +1,27 @@
+let age = 2
+
+if (age > 18) {
+    console.log("allowed to Play!");
+} else {
+    console.log("not allowed to play")
+}
+
+let AgeGirl = 12
+let AgeBoy = 23
+
+if (AgeGirl >= 18 && AgeBoy >= 21) {
+    console.log("Allowed to marriage")
+} else {
+    console.log("Not allowed to marriage, Call Police!")
+
+}
+
+let num = 5
+
+if (num == 0) {
+    console.log("number is zero")
+} else if (num % 2 == 0) {
+    console.log("even number");
+} else {
+    console.log("odd number");
+}
