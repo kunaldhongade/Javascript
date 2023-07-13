b4bef74afca07e7bd505cf4e679483845755c3d7
kunaldhongade <kunaldhongade1@gmail.com>
2023-07-13 13:31:25 +0530

    fill method

---
+++ 57_fill.js
@@ -0,0 +1,10 @@
+// fill method in js
+// value, start , end
+
+const myArray = new Array(10).fill(-1)
+console.log(myArray)
+
+const arr8 = [1, 2, 3, 4, 5, 6, 7, 8]
+arr8.fill(0, 2, 5)// fill changes original  array
+
+console.log(arr8)
