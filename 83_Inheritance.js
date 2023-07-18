350a6d735db4a8734172a32193595eb8582ef9b7
kunaldhongade <kunaldhongade1@gmail.com>
2023-07-18 11:00:45 +0530

    Getter and Setter

---
+++ 83_Inheritance.js
@@ -0,0 +1,42 @@
+class Animal {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+
+    eat() {
+        return `${this.name} is eating`
+    }
+
+    isAdult() {
+        return this.age >= 2
+    }
+
+    isMammal() {
+        return true
+    }
+}
+
+// this is inheritance
+
+class Dog extends Animal {
+    constructor(name, age, speed) {
+        super(name, age)
+        this.speed = speed
+    }
+
+    eat() {
+        return `Modified Eat: ${this.name} is eating`
+    }
+    run() {
+        return `${this.name} is running at ${this.speed} kmph`
+    }
+}
+
+// inheritance kelyavar jar sub class madhe constructor nasel tar parent class cha constructor call kela jato
+// aani methods sub class madhe nasel tar parent / super class madhe search karto
+// object / instance are same
+const tommy = new Dog("tommy", 3, 45)
+console.log(tommy.isAdult())
+console.log(tommy.run())
+console.log(tommy.eat(-3))
