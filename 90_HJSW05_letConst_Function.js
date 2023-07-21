27fff1209b60304c86af8084c63f370d24bc3f3d
kunaldhongade <kunaldhongade1@gmail.com>
2023-07-21 12:15:34 +0530

    How Js works 05 Let Hoisting

--- 90_HJSW05_letConst_Function.js
+++ 90_HJSW05_letConst_Function.js
@@ -1,4 +1,5 @@
 
+console.log(typeof firstName) // reference err
 console.log(firstName)
 let firstName = "kunal"
 console.log(firstName)
@@ -22,6 +23,31 @@ console.log(firstName)
  *  
  *      1. console.log(firstName)
  *              => gives us err uncaught err not accessible
- *      2. 
- *  
+ * 
+ *      2. let firstName = "kunal"
+ *              => initialize firstName
+ * 
+ *      3. console.log(firstName)
+ * 
+ * Temporal Dead Zone (TDZ)
+ * 
+ *      - at the time of starting value of firstName's value is uninitialized
+ *      - after assigning value to firstName then it's initialized
+ *      - until the variable gets initialized it will be in TDZ and gives Reference ERror
+ * 
+ * but we can't do this thing with const
+ * 
+ * 
+ */
+
+
+/**
+ *  1. Compilation Phase
+ *          i. error checking
+ *          ii. scope of var
+ * 
+ *  2. Code Execution Phase
+ *          In order to execute Js code Js creates execution context
+ *              i. Creation Phase -> Memory Create (Hoisting)
+ *              ii. Line by line Execution 
  */
