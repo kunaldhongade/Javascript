79e1358e43e4de4d48760535de9b03fbbcbcb3c3
kunaldhongade <kunaldhongade1@gmail.com>
2023-07-23 15:49:05 +0530

    How Js work's 06 Compilation

---
+++ 91_HJSW06_Compilation.js
@@ -0,0 +1,18 @@
+console.log("Hello World")
+let firstName = "kunal"
+let lastName = "Dhongade"
+
+const myFunction = function () {
+    let var1 = "First Variable"
+    let var2 = "second Variable"
+
+    console.log(var1)
+    console.log(var2)
+}
+
+/**
+ *      1. Global Scope
+ *          - Js knows whats in the code before compilation / execution
+ *          - scope decided
+ *      
+ */
