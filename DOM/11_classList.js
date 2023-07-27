ab8787e8ba5cee299ac5f843bc1cf15c4f2afea8
kunaldhongade <kunaldhongade1@gmail.com>
2023-07-27 14:02:09 +0530

    Class List

--- DOM/11_classList.js
+++ DOM/11_classList.js
@@ -1,4 +1,22 @@
-// to get all classes set to perticular element
+// to get all classes set to particular element
 
 const sectionTodo = document.querySelector(".section-todo")
-console.log(sectionTodo)
+
+sectionTodo.classList.add('borderSection')
+sectionTodo.classList.remove('container')
+const classExists = sectionTodo.classList.contains('section-todo')
+
+console.log(sectionTodo.classList, classExists)
+
+// toggle checks whether the class exist or not
+// if exist then remove and if not exist then add
+
+sectionTodo.classList.toggle("borderSection")
+sectionTodo.classList.toggle("container")
+
+
+// selecting header
+
+const header = document.querySelector(".header")
+console.log(header.classList)
+header.classList.add("borderSection")
