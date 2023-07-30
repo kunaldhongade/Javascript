b744b76170f7dc9a21ff9afcd8f0d435a233a57d
kunaldhongade <kunaldhongade1@gmail.com>
2023-07-31 00:10:08 +0530

    event Deligation

--- DOM/29_eventDeligation.js
+++ DOM/29_eventDeligation.js
@@ -3,12 +3,15 @@
 const grandparent = document.querySelector(".grandparent")
 
 grandparent.addEventListener("click", (event) => {
-    console.log("you clicked grandparent")
+    // console.log(event.target) //returns original clicked div
+    console.log(event.target.textContent) // returns contains inside that box and boxes eg. if we clicked on child --> child
+    // parent --> parent child
+    // grandparent --> grandparent parent child
 })
 
 /*
     when clicked on child but bubbling happens can capture event
     above it
     shape of U
-    
+
 */
